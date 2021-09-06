e60214fba2fa16bef7a7c7fb2f439b175845bb56
Orlando <luisorlandogomezjimenez@gmail.com>
2021-09-06 18:04:59 -0500

    MERN STACK project

---
+++ backend/src/controllers/user.controllers.js
@@ -0,0 +1,23 @@
+
+
+const userCtrl = {};
+
+const User = require('../models/User');
+
+userCtrl.getUsers =  async(req, res) => {
+    const users = await User.find();
+    res.json(users)
+};
+userCtrl.createUser =   async(req, res) => {
+    const {username } = req.body;
+    const newUser = new User({username});
+    await newUser.save();
+    res.json('User created')
+};
+userCtrl.deleteUser = async (req, res) => {
+    const { id } = req.params;
+    await User.findByIdAndDelete(id);
+    res.json('User deleted');
+}
+
+module.exports = userCtrl;
