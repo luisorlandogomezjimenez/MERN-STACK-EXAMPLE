41e5245f1a32a2bb5539be4dbef8e68ccf3fe001
Orlando <luisorlandogomezjimenez@gmail.com>
2021-09-12 12:30:26 -0500

    finalized

--- backend/src/database.js
+++ backend/src/database.js
@@ -6,9 +6,7 @@ const URI = process.env.MONGODB_URI
 
 mongoose.connect(URI, {
   useNewUrlParser: true,
-  useCreateIndex: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
 });
 
 const connection = mongoose.connection;
